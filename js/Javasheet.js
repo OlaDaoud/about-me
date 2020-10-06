fe087da2fa910c29c32f1b9ba98b2eead2439d00
OlaDaoud <oladaoud1996@gmail.com>
2020-10-06 18:06:59 +0300

    loops

--- js/Javasheet.js
+++ js/Javasheet.js
@@ -1,7 +1,10 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-redeclare */
 /* eslint-disable no-unused-vars */
 'use strict';
 var username= prompt('Hi, I am Ola,and who you are ?');
-console.log(name);
+console.log(username);
+var score=0 ;
 
 var welcome=confirm('Are you ready to know me better?');
 
@@ -14,6 +17,7 @@ switch(color)
 case 'blue':
   console.log(color2);
   alert('Great! You are doing well');
+  score++;
   break;
 default:
   console.log(color2);
@@ -36,10 +40,12 @@ case 'y':
 case 'no':
   console.log('no');
   alert('You are right, I owe you a hanmade gift');
+  score++;
   break;
 case 'n':
   console.log('no');
   alert('You are right, I owe you a hanmade gift');
+  score++;
   break;
 }
 
@@ -52,10 +58,12 @@ switch(activity2)
 case 'yes':
   console.log('yes');
   alert('Bravooo!I am a night owl');
+  score++;
   break;
 case 'y':
   console.log('yes');
   alert('Bravooo!I am a night owl');
+  score++;
   break;
 case 'no':
   console.log('no');
@@ -84,10 +92,12 @@ case 'y':
 case 'no':
   console.log('no');
   alert(' You know me well ! but, as a good friend you have to be supportive ^^ ');
+  score++;
   break;
 case 'n':
   console.log('no');
   alert('You know me well ! but, as a good friend you have to be supportive ^^ ');
+  score++;
   break;
 }
 
@@ -107,14 +117,74 @@ case 'y':
 case 'no':
   console.log('no');
   alert(' Yaaas, My HUAWEI is happy to hear your voice anytime ');
+  score++;
   break;
 case 'n':
   console.log('no');
   alert('Yaaas, My HUAWEI is happy to hear your voice anytime ');
+  score++;
   break;
 }
 
-alert('Thank you' +name+', even If you did\'nt answer everything correctly I am very blissed to have such a caring friend ! ');
+
+
+// var i=0 ;
+//   i++;
+//   alert(i);
+
+//  else if (year<1996){
+//     console.log('too low');
+//     alert('too low , I am older');
+//     break;}
+//   case (year>1996):
+//     console.log('too high');
+//     alert(' too high, I am younger ');
+//     break;
+//   case (year===1996):
+//     console.log('1996');
+//     alert('yes this is the lucky year ');
+//     score++;
+//     break;
+//   }
+// }
+for (var i=0; i<4; i++){
+  var age= prompt('In which year I was born ?');
+
+
+  if (age < 1996){
+    console.log('too high');
+    alert('too high , I am younger');
+  }else if (age > 1996){
+    console.log('too low');
+    alert(' too low, I am older ');
+
+  }
+  else{
+    alert ('correct answer'+ age);
+    score++;
+    break;
+  }
+
+}
+
+var movies=['nemo','brave','up','frozen','lion king','inside out','big hero'];
+for( var i=0; i<6 ; i++){
+  var moviesq= prompt(' name one of my fav disney movies : ');
+
+  if (moviesq == ('nemo'||'brave'||'up'||'frozen'||'lion king'||'inside out'||'big hero')){
+    console.log('correct');
+    alert(' Niiceeeeee, when we will watch it together ? ');
+    score++;
+    break;
+  }
+  else{
+    console.log=('wrong');
+    alert(' Sorry, thats not my type ..');
+  }
+}
+alert( 'those are my fav movies ' + movies);
+
+alert('Thank you'+' ' +username+' '+'your score is'+' '+score+' '+', even If you did\'nt answer everything correctly I am very blissed to have such a caring friend ! ');
 
 
 
