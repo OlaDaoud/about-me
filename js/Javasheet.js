5f32210908eb3e2dddfa1507bfa6718302d333c0
MHD22 <mhd.sad22@gmail.com>
2020-10-07 14:35:42 +0300

    convert to functions

--- js/Javasheet.js
+++ js/Javasheet.js
@@ -7,73 +7,80 @@ console.log(username);
 var score=0 ;
 
 var welcome=confirm('Are you ready to know me better?');
-
-var color= prompt('which color do I love the most?');
-var color2=color.toLowerCase();
-console.log(color2);
-
-switch(color)
-{
-case 'blue':
+function colorQuestion(){
+  var color= prompt('which color do I love the most?');
+  var color2=color.toLowerCase();
   console.log(color2);
-  alert('Great! You are doing well');
-  score++;
-  break;
-default:
-  console.log(color2);
-  alert ('mmm I don\'t like it');
+  
+  switch(color2)
+  {
+  case 'blue':
+    console.log(color2);
+    alert('Great! You are doing well');
+    score++;
+    break;
+  default:
+    console.log(color2);
+    alert ('mmm I don\'t like it');
+  }
 }
+colorQuestion();
 
-
-var handcraft= prompt('Is it right that I am bad at handcrafts ?');
-var handcraft2=handcraft.toLowerCase();
-switch(handcraft2)
-{
-case 'yes':
-  console.log('yes');
-  alert('Ooops, It looks like you didn\'t get any handmade gift from me ? ');
-  break;
-case 'y':
-  console.log('yes');
-  alert('Ooops, It looks like you didn\'t get any handmade gift from me ?');
-  break;
-case 'no':
-  console.log('no');
-  alert('You are right, I owe you a hanmade gift');
-  score++;
-  break;
-case 'n':
-  console.log('no');
-  alert('You are right, I owe you a hanmade gift');
-  score++;
-  break;
+function handcraftQuestion(){
+  var handcraft= prompt('Is it right that I am bad at handcrafts ?');
+  var handcraft2=handcraft.toLowerCase();
+  switch(handcraft2)
+  {
+  case 'yes':
+    console.log('yes');
+    alert('Ooops, It looks like you didn\'t get any handmade gift from me ? ');
+    break;
+  case 'y':
+    console.log('yes');
+    alert('Ooops, It looks like you didn\'t get any handmade gift from me ?');
+    break;
+  case 'no':
+    console.log('no');
+    alert('You are right, I owe you a hanmade gift');
+    score++;
+    break;
+  case 'n':
+    console.log('no');
+    alert('You are right, I owe you a hanmade gift');
+    score++;
+    break;
+  }
 }
+handcraftQuestion();
 
 
 var activity= prompt('Ola is a night preson ?');
 var activity2=activity.toLowerCase();
+function activityQuestion(){
 
-switch(activity2)
-{
-case 'yes':
-  console.log('yes');
-  alert('Bravooo!I am a night owl');
-  score++;
-  break;
-case 'y':
-  console.log('yes');
-  alert('Bravooo!I am a night owl');
-  score++;
-  break;
-case 'no':
-  console.log('no');
-  alert('mmm for your safety,you better avoid me before 12 pm ');
-  break;
-case 'n':
-  console.log('no');
-  alert('mmm for your safety, you better avoid me before 12 pm ');
-  break;
+  switch(activity2)
+  {
+  case 'yes':
+    console.log('yes');
+    alert('Bravooo!I am a night owl');
+    score++;
+    break;
+  case 'y':
+    console.log('yes');
+    alert('Bravooo!I am a night owl');
+    score++;
+    break;
+  case 'no':
+    console.log('no');
+    alert('mmm for your safety,you better avoid me before 12 pm ');
+    break;
+  case 'n':
+    console.log('no');
+    alert('mmm for your safety, you better avoid me before 12 pm ');
+    break;
+  }
 }
+activityQuestion();
 
 
 var music= prompt('Can I play any kind of music ?');
